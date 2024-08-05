6afb1d3814c4d5d22c650db8f32516d7f5105785
gleblan1 <kekskiylol@gmail.com>
2024-08-05 15:12:59 +0200

    trimed file naming

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { Header } from "../Header/Header";
 import { Outlet, useNavigate } from "react-router-dom";
 import { Footer } from "../Footer/Footer";
-import { Loading } from "../Loading /Loading";
+import { Loading } from "../Loading/Loading";
 import { useLoading } from "../../services/loadingService/loadingService";
 import { useClasses } from "./Layout.style";
 
